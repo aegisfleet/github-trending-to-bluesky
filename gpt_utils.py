3b23b8d6b9996b573c1d51d0d58696ee3531ceb3
aegisfleet <aegisfleet@gmail.com>
2024-04-22 21:28:09 +0900

    リファクタリング

--- gpt_utils.py
+++ gpt_utils.py
@@ -8,30 +8,43 @@ def setup_cookies():
         "_U": "cookie value"
     })
 
-def get_description(gpt_client, repo_name, text, max_retries=3):
+def get_description(gpt_client, text, max_retries=3):
     def attempt_request(retry_count):
         try:
             response = gpt_client.chat.completions.create(
                 model="gpt-4-turbo",
                 provider=g4f.Provider.Bing,
-                messages=[{"role": "user", "content": f"{repo_name}リポジトリは誰がいつどこで使うものか250文字以下で3行にまとめて欲しい。\n回答は日本語で強調文字は使用せず簡素にする。\n以下にリポジトリのREADMEを記載する。\n\n{text}"}],
+                messages=[
+                    {"role": "user", "content": f"{text}"}
+                ],
             )
             content = response.choices[0].message.content
             if len(content) > 250:
-                raise ValueError("レスポンスの文字数が250文字を超えています。")
+                raise ValueError(
+                    "レスポンスの文字数が250文字を超えています。"
+                )
             return content
         except RateLimitError:
-            print(f"RateLimitErrorが発生しました。リトライ回数: {retry_count}")
+            print(
+                f"RateLimitErrorが発生しました。リトライ回数: {retry_count}"
+            )
             time.sleep(30)
         except ValueError:
-            print(f"ValueErrorが発生しました。レスポンスの文字数が250文字を超えています。リトライ回数: {retry_count}\n{content}")
+            print(
+                f"ValueErrorが発生しました。レスポンスの文字数が250文字を超えています。"
+                f"リトライ回数: {retry_count}\n{content}"
+            )
             time.sleep(3)
         except Exception as e:
-            print(f"予期しないエラーが発生しました。リトライ回数: {retry_count}\nエラータイプ: {type(e).__name__}\nエラーメッセージ: {e}")
+            print(
+                f"予期しないエラーが発生しました。リトライ回数: {retry_count}\n"
+                f"エラータイプ: {type(e).__name__}\nエラーメッセージ: {e}"
+            )
             time.sleep(30)
-            if retry_count < max_retries:
-                return attempt_request(retry_count + 1)
-            else:
-                raise Exception("最大リトライ回数に達しました。")
+
+        if retry_count < max_retries:
+            return attempt_request(retry_count + 1)
+        else:
+            raise Exception("最大リトライ回数に達しました。")
 
     return attempt_request(0)
--- main.py
+++ main.py
@@ -30,9 +30,16 @@ def main():
         retries = 0
         max_retries = 3
         while retries < max_retries:
-            message = gpt_utils.get_description(gpt_client, repo_name, readme_text)
+            message = gpt_utils.get_description(
+                gpt_client, 
+                f"{repo_name}リポジトリは誰がいつどこで使うものか"
+                "250文字以下で3行にまとめて欲しい。\n回答は日本語で強調文字は使用せず簡素にする。"
+                f"\n以下にリポジトリのREADMEを記載する。\n\n{readme_text}"
+            )
             title, description, image_url = bluesky_utils.fetch_webpage_metadata(full_url)
-            post_text = bluesky_utils.format_message_with_link(repo_name, full_url, "今日のGitHubトレンド", message)
+            post_text = bluesky_utils.format_message_with_link(
+                repo_name, full_url, "今日のGitHubトレンド", message
+            )
 
             if len(post_text.build_text()) < 300:
                 break
@@ -47,7 +54,9 @@ def main():
         print(post_text.build_text(), image_url, sep="\n")
 
         bluesky_utils.authenticate(bs_client, user_handle, user_password)
-        embed_external = bluesky_utils.create_external_embed(bs_client, title, description, full_url, image_url)
+        embed_external = bluesky_utils.create_external_embed(
+            bs_client, title, description, full_url, image_url
+        )
         bluesky_utils.post(bs_client, post_text, embed_external)
 
 if __name__ == "__main__":
